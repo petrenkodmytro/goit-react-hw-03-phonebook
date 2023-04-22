3314edcb5d108a5c934c919da8162435d4955d2a
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-04-22 20:30:27 +0300

    add json file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import initialContacts from './contacts.json';
 
 const notificationMassege = 'is already in contacts!';
 const notificationOptions = {
@@ -21,12 +22,7 @@ const notificationOptions = {
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initialContacts,
     filter: '',
   };
 
